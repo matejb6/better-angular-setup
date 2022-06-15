eb02e68bc5a8b5de8e45e1d8b0a39eaf45d7bc4d
matej <matejb6@icloud.com>
2022-06-15 07:36:15 +0200

    toolbar unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [AppModule],
       declarations: [AppComponent]
     }).compileComponents();
   });
---
+++ src/app/core/layout/toolbar/toolbar.component.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CoreModule } from '@core/core.module';
+import { ToolbarComponent } from './toolbar.component';
+
+describe('ToolbarComponent', () => {
+  let fixture: ComponentFixture<ToolbarComponent>;
+  let component: ToolbarComponent;
+  let nativeElem: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CoreModule]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    nativeElem = fixture.nativeElement;
+  });
+
+  it('should be defined', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should render toolbar', () => {
+    expect(getToolbar(nativeElem)).toBeTruthy();
+  });
+
+  it('should render toolbar title', () => {
+    expect(getToolbarTitle(nativeElem)).toBeTruthy();
+  });
+});
+
+const getToolbar = (parentElem: HTMLElement): HTMLElement | null => parentElem.querySelector('mat-toolbar');
+
+const getToolbarTitle = (parentElem: HTMLElement): HTMLElement | null | undefined =>
+  getToolbar(parentElem)?.querySelector('span');
