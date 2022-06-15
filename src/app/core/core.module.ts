1d911f9821a5b8c978dea4e628a7a9ee6931bcc2
matej <matejb6@icloud.com>
2022-06-15 07:23:25 +0200

    toolbar angular logo added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { ToolbarComponent } from './layout/toolbar/toolbar.component';
 
 @NgModule({
-  imports: [MatToolbarModule],
+  imports: [HttpClientModule, MatIconModule, MatToolbarModule],
   exports: [ToolbarComponent],
   declarations: [ToolbarComponent],
   providers: []
--- src/app/core/layout/toolbar/toolbar.component.ts
+++ src/app/core/layout/toolbar/toolbar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'angular-logo',
+      sanitizer.bypassSecurityTrustResourceUrl('../../../../assets/logos/angular_solidBlack.svg')
+    );
+  }
+}
