108c12d8c11e44d071de676d20e8c7ba04445ad1
matej <matejb6@icloud.com>
2022-06-16 20:40:52 +0200

    libraries file

--- src/app/views/welcome-view/welcome-view.component.ts
+++ src/app/views/welcome-view/welcome-view.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { libraries } from './libraries';
 import { LibraryCard } from '@shared/components/library-card/library-card.component';
 
 @Component({
@@ -16,50 +17,7 @@ export class WelcomeViewComponent {
   constructor() {
     this.title = 'Hey developer';
     this.paragraph = 'This is custom Angular app setup which includes:';
-    this.libraries = [
-      {
-        logoPath: '../../../assets/logos/router.svg',
-        name: 'Angular Router',
-        description: 'Angular Router package',
-        link: 'https://angular.io/guide/routing-overview'
-      },
-      {
-        logoPath: '../../../assets/logos/material.svg',
-        name: 'Angular Material',
-        description: 'Material Design components for Angular',
-        link: 'https://material.angular.io'
-      },
-      {
-        logoPath: '../../../assets/logos/eslint.svg',
-        name: 'ESLint',
-        description: 'Find and fix problems in your JavaScript code',
-        link: 'https://eslint.org'
-      },
-      {
-        logoPath: '../../../assets/logos/github.png',
-        name: 'GitHub Actions',
-        description: 'Automate your workflow from idea to production',
-        link: 'https://github.com/features/actions'
-      },
-      {
-        logoPath: '../../../assets/logos/prettier.png',
-        name: 'Prettier',
-        description: 'An opinionated code formatter',
-        link: 'https://prettier.io'
-      },
-      {
-        logoPath: '',
-        name: 'Husky git hooks',
-        description: 'Modern native git hooks made easy',
-        link: 'https://typicode.github.io/husky/#/'
-      },
-      {
-        logoPath: '../../../assets/logos/karma.svg',
-        name: 'Karma config',
-        description: 'Karma config extended with spec reporter and CI setup',
-        link: 'https://karma-runner.github.io/latest/index.html'
-      }
-    ];
+    this.libraries = libraries;
     this.message = 'This is just the beginning, it is your turn now :)...';
   }
 }
