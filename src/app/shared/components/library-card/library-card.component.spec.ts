6a9a137245e58ac5fa04579786774ddfed85fc3c
matej <matejb6@icloud.com>
2022-06-16 20:35:30 +0200

    library cad unit tests extended

--- src/app/shared/components/library-card/library-card.component.spec.ts
+++ src/app/shared/components/library-card/library-card.component.spec.ts
@@ -1,12 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SharedModule } from '@shared/shared.module';
-import { LibraryCardComponent } from './library-card.component';
+import { LibraryCard, LibraryCardComponent } from './library-card.component';
 
 describe('LibraryCardComponent', () => {
   let fixture: ComponentFixture<LibraryCardComponent>;
   let component: LibraryCardComponent;
   let nativeElem: HTMLElement;
+
+  const libraryCard: LibraryCard = {
+    logoPath: '',
+    name: 'Library',
+    description: 'Super awesome library you can use',
+    link: 'https://www.example.com/link'
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,6 +24,7 @@ describe('LibraryCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LibraryCardComponent);
     component = fixture.componentInstance;
+    component.libraryCard = libraryCard;
     fixture.detectChanges();
     nativeElem = fixture.nativeElement;
   });
@@ -37,8 +45,28 @@ describe('LibraryCardComponent', () => {
     expect(getCardImage(nativeElem)).toBeTruthy();
   });
 
-  it('should render card content', () => {
-    expect(getCardContent(nativeElem)).toBeTruthy();
+  it('should render card content description', () => {
+    expect(getCardContentDescription(nativeElem)).toBeTruthy();
+  });
+
+  it('should render card content link button', () => {
+    expect(getCardContentLinkButton(nativeElem)).toBeTruthy();
+  });
+
+  it('should have title', () => {
+    expect(getCardTitle(nativeElem)?.innerText).toEqual(libraryCard.name);
+  });
+
+  it('should have description', () => {
+    expect(getCardContentDescription(nativeElem)?.innerText).toEqual('Super awesome library you can use');
+  });
+
+  it('should have link button text', () => {
+    expect(getCardContentLinkButton(nativeElem)?.innerText).toEqual('More Info');
+  });
+
+  it('should have link button link', () => {
+    expect(getCardContentLinkButton(nativeElem)?.href).toEqual('https://www.example.com/link');
   });
 });
 
@@ -47,8 +75,14 @@ const getCard = (parentElem: HTMLElement): HTMLElement | null => parentElem.quer
 const getCardTitle = (parentElem: HTMLElement): HTMLElement | null | undefined =>
   getCard(parentElem)?.querySelector('mat-card-title');
 
-const getCardImage = (parentElem: HTMLElement): HTMLElement | null | undefined =>
-  getCard(parentElem)?.querySelector('img');
+const getCardImage = (parentElem: HTMLElement): HTMLImageElement | null | undefined =>
+  getCard(parentElem)?.querySelector('img.logo');
 
 const getCardContent = (parentElem: HTMLElement): HTMLElement | null | undefined =>
   getCard(parentElem)?.querySelector('mat-card-content');
+
+const getCardContentDescription = (parentElem: HTMLElement): HTMLParagraphElement | null | undefined =>
+  getCardContent(parentElem)?.querySelector('p');
+
+const getCardContentLinkButton = (parentElem: HTMLElement): HTMLAnchorElement | null | undefined =>
+  getCardContent(parentElem)?.querySelector('a');
