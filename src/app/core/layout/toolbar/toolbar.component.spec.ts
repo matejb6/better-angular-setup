01bf63abfa3395f101f29fe44596ae9b5c7bf663
matej <matejb6@icloud.com>
2022-06-16 20:12:01 +0200

    toolbar component unit tests

--- src/app/core/layout/toolbar/toolbar.component.spec.ts
+++ src/app/core/layout/toolbar/toolbar.component.spec.ts
@@ -29,12 +29,19 @@ describe('ToolbarComponent', () => {
     expect(getToolbar(nativeElem)).toBeTruthy();
   });
 
+  it('should render toolbar icon', () => {
+    expect(getToolbarIcon(nativeElem)).toBeTruthy();
+  });
+
   it('should render toolbar title', () => {
     expect(getToolbarTitle(nativeElem)).toBeTruthy();
   });
 });
 
 const getToolbar = (parentElem: HTMLElement): HTMLElement | null => parentElem.querySelector('mat-toolbar');
+
+const getToolbarIcon = (parentElem: HTMLElement): HTMLElement | null | undefined =>
+  getToolbar(parentElem)?.querySelector('mat-icon.mat-icon');
 
 const getToolbarTitle = (parentElem: HTMLElement): HTMLElement | null | undefined =>
   getToolbar(parentElem)?.querySelector('span');
