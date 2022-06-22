b06bebe515c3f41d7dad95b3eec978c095c4e392
matej <matejb6@icloud.com>
2022-06-22 17:16:09 +0200

    library card content change, ripple added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 
 import { LibraryCardComponent } from './components/library-card/library-card.component';
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatCardModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatRippleModule],
   exports: [CommonModule, LibraryCardComponent],
   declarations: [LibraryCardComponent],
   providers: []
