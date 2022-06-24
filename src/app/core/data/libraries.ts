b4fc95920b7787cf7ed5f42ccbf06644e7088551
matej <matejb6@icloud.com>
2022-06-24 14:04:06 +0200

    libraries steps to add updated

--- src/app/core/data/libraries.ts
+++ src/app/core/data/libraries.ts
@@ -7,8 +7,7 @@ export const libraries: LibraryCard[] = [
     description: 'Angular Router package',
     link: 'https://angular.io/guide/routing-overview',
     stepsToAdd: [
-      'Create new Angular app with command: ng new app-name',
-      'Accept Angular Router when prompted',
+      'If creating new app, accept Angular Router when prompted',
       'If adding to existing app, install package with: npm install @angular/router'
     ]
   },
@@ -42,20 +41,35 @@ export const libraries: LibraryCard[] = [
     name: 'Prettier',
     description: 'An opinionated code formatter',
     link: 'https://prettier.io',
-    stepsToAdd: []
+    stepsToAdd: [
+      'Install Prettier with command: npm install prettier --save-dev',
+      'If ESLint is installed, install eslint-config-prettier: npm install eslint-config-prettier --save-dev',
+      'In root, create file .prettierignore and add all files and folders which should be ignored',
+      'In root, create file .prettierrc.json and add rules you want to override',
+      'Check code format with command: npx prettier --check',
+      'Format code with command: npx prettier --write'
+    ]
   },
   {
     logoPath: '',
     name: 'Husky git hooks',
     description: 'Modern native git hooks made easy',
     link: 'https://typicode.github.io/husky/#/',
-    stepsToAdd: []
+    stepsToAdd: [
+      'Install Husky with command: npm install husky --save-dev',
+      'Init husky with command: npx husky install',
+      'In root create file .husky/pre-commit containing commands which should be run'
+    ]
   },
   {
     logoPath: '../../../assets/logos/karma.svg',
     name: 'Karma config',
     description: 'Karma config extended with spec reporter and CI setup',
     link: 'https://karma-runner.github.io/latest/index.html',
-    stepsToAdd: []
+    stepsToAdd: [
+      'Check karma.conf.js for Karma configuration',
+      'Check package.json test scripts',
+      'Install Karma spec reporter with command: npm install karma-spec-reporter --save-dev'
+    ]
   }
 ];
