e223597478d15663b846bd94c2f9507ec1ec7e22
matej <matejb6@icloud.com>
2022-06-24 15:25:58 +0200

    welcome view unit tests added

--- src/app/views/welcome-view/welcome-view.component.spec.ts
+++ src/app/views/welcome-view/welcome-view.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { WelcomeViewModule } from './welcome-view.module';
 import { WelcomeViewComponent } from './welcome-view.component';
@@ -10,7 +11,7 @@ describe('WelcomeViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WelcomeViewModule]
+      imports: [BrowserAnimationsModule, WelcomeViewModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(WelcomeViewComponent);
@@ -23,18 +24,44 @@ describe('WelcomeViewComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have title', () => {
-    expect(component.title).toContain('Hey developer');
-  });
-
   it('should render title', () => {
-    expect(nativeElem.querySelector('h1')).toBeTruthy();
+    expect(getTitle(nativeElem)).toBeTruthy();
   });
 
   it('should render multiple library cards', () => {
     expect(getLibraryCards(nativeElem).length).toBeGreaterThan(0);
   });
+
+  it('should have title', () => {
+    expect(getTitle(nativeElem)?.innerText).toContain('Hey developer');
+  });
+
+  it('should open dialog on card click', () => {
+    getFirstLibraryCard(nativeElem)?.click();
+
+    expect(getLibraryDialog()).toBeTruthy();
+  });
+
+  it('should close dialog on OK button click', (done) => {
+    getFirstLibraryCard(nativeElem)?.click();
+    getLibraryDialogOkButton()?.click();
+
+    setTimeout(() => {
+      expect(getLibraryDialog()).toBeFalsy();
+      done();
+    }, 500);
+  });
 });
+
+const getTitle = (parentElem: HTMLElement): HTMLHeadingElement | null => parentElem.querySelector('h1');
 
 const getLibraryCards = (parentElem: HTMLElement): NodeListOf<HTMLElement> =>
   parentElem.querySelectorAll('app-library-card');
+
+const getFirstLibraryCard = (parentElem: HTMLElement): HTMLElement | null =>
+  getLibraryCards(parentElem).item(0).querySelector('mat-card.card');
+
+const getLibraryDialog = (): Element | null => document.querySelector('app-library-dialog');
+
+const getLibraryDialogOkButton = (): HTMLButtonElement | null | undefined =>
+  getLibraryDialog()?.querySelector('button.mat-primary');
