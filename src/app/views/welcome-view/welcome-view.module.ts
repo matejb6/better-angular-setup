3680c743c108a04693ef213c35e250b67ec67dbd
matej <matejb6@icloud.com>
2022-08-24 14:32:13 +0200

    Remove welcome view service

--- src/app/views/welcome-view/welcome-view.module.ts
+++ src/app/views/welcome-view/welcome-view.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { WelcomeViewRoutingModule } from './welcome-view-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { WelcomeViewComponent } from './welcome-view.component';
-import { WelcomeViewService } from './welcome-view.service';
 
 @NgModule({
   imports: [WelcomeViewRoutingModule, SharedModule],
   exports: [],
   declarations: [WelcomeViewComponent],
-  providers: [WelcomeViewService]
+  providers: []
 })
 export class WelcomeViewModule {}
