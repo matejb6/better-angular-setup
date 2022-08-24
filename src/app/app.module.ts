91db46bbffa3d7f27d10cfccc971ba522714b7f3
matej <matejb6@icloud.com>
2022-08-24 14:56:50 +0200

    Move layout module to its own folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@core/core.module';
+import { LayoutModule } from '@layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
