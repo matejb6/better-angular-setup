e4f134c08a64a68e964c022d6c43b5c6de68aee1
matej <matejb6@icloud.com>
2022-08-24 14:51:33 +0200

    Refactor view routing modules setup

--- src/app/views/page-not-found-view/page-not-found-view-routing.module.ts
+++ src/app/views/page-not-found-view/page-not-found-view-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { PageNotFoundViewComponent } from './page-not-found-view.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: PageNotFoundViewComponent
-  }
-];
-
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        component: PageNotFoundViewComponent
+      }
+    ])
+  ],
   exports: [RouterModule]
 })
 export class PageNotFoundViewRoutingModule {}
--- src/app/views/welcome-view/welcome-view-routing.module.ts
+++ src/app/views/welcome-view/welcome-view-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { WelcomeViewComponent } from './welcome-view.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: WelcomeViewComponent
-  }
-];
-
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        component: WelcomeViewComponent
+      }
+    ])
+  ],
   exports: [RouterModule]
 })
 export class WelcomeViewRoutingModule {}
