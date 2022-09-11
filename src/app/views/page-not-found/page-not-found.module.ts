60ebbec1fbcd64d6ee18639604038e1f20264a7d
matej <matejb6@icloud.com>
2022-09-11 11:27:04 +0200

    Remove unused module import from page not found module

--- src/app/views/page-not-found/page-not-found.module.ts
+++ src/app/views/page-not-found/page-not-found.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from '@shared/shared.module';
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
-  imports: [SharedModule, PageNotFoundRoutingModule],
+  imports: [PageNotFoundRoutingModule],
   exports: [],
   declarations: [PageNotFoundComponent],
   providers: []
