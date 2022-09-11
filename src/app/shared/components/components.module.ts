a176d2402d1712060756cd0fd77984bfdefe63df
matej <matejb6@icloud.com>
2022-09-11 11:50:29 +0200

    Add library card module

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { MatRippleModule } from '@angular/material/core';
 
-import { LibraryCardComponent } from './library-card/library-card.component';
+import { LibraryCardModule } from './library-card/library-card.module';
 import { LibraryDialogComponent } from './library-dialog/library-dialog.component';
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatRippleModule],
-  exports: [LibraryCardComponent, LibraryDialogComponent],
-  declarations: [LibraryCardComponent, LibraryDialogComponent],
+  imports: [CommonModule, MatButtonModule, MatDialogModule, MatIconModule, LibraryCardModule],
+  exports: [LibraryDialogComponent, LibraryCardModule],
+  declarations: [LibraryDialogComponent],
   providers: []
 })
 export class ComponentsModule {}
