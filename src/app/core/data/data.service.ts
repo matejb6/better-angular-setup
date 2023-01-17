bc4936b63f4608fe0f1c99349a66856140062a51
Matej Buljan <matejb6@icloud.com>
2023-01-17 11:43:05 +0100

    Add data service

---
+++ src/app/core/data/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { Library } from '@core/interfaces/library';
+import { libraries } from './libraries';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private librariesBehaviorSubject: BehaviorSubject<Library[]> = new BehaviorSubject<Library[]>(libraries);
+
+  constructor() {}
+
+  /**
+   * Returns libraries behavior subject as observable
+   * @returns Libraries observable
+   */
+  public getLibrariesObs(): Observable<Library[]> {
+    return this.librariesBehaviorSubject.asObservable();
+  }
+}
