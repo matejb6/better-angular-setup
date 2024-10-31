9dcf08491dfcdbeb81b95a40e0c78ab975d77495
Matej Buljan <matejb6@icloud.com>
2024-10-31 10:52:18 +0100

    Rename views into pages

--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -7,11 +7,11 @@ import { LibraryDialogComponent } from '@shared/components/library-dialog/librar
 import { DialogService } from '@shared/services/dialog/dialog.service';
 
 @Component({
-  selector: 'app-welcome-view',
-  templateUrl: './welcome-view.component.html',
-  styleUrl: './welcome-view.component.scss'
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrl: './welcome-page.component.scss'
 })
-export class WelcomeViewComponent {
+export class WelcomePageComponent {
   public readonly title: string = 'Hey developer';
   public readonly paragraph: string = 'This is custom Angular app setup which includes:';
   public readonly message: string = 'This is just the beginning, it is your turn now :)...';
