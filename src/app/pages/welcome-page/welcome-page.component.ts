7a82f39dff5c43f91f23eb5e0f6e336ecfb86c29
Matej Buljan <matejb6@icloud.com>
2024-11-09 10:54:57 +0100

    Update welcome page texts

--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -12,9 +12,9 @@ import { DialogService } from '@shared/services/dialog/dialog.service';
   styleUrl: './welcome-page.component.scss'
 })
 export class WelcomePageComponent {
-  public readonly title: string = 'Hey developer';
-  public readonly paragraph: string = 'This is an Angular app with custom setup which includes:';
-  public readonly message: string = 'This is just the beginning, it is your turn now :)...';
+  public readonly title: string = 'Hey developer 👋';
+  public readonly paragraph: string = 'This is an Angular app with additional setup which includes:';
+  public readonly message: string = 'This is just the beginning, it is your turn now 🙂...';
   public $libraries: Observable<Library[]> = this.dataService.getLibrariesObs();
 
   constructor(
