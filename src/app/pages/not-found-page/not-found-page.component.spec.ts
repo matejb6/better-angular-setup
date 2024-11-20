827972f0e047e0624199ef3b39d7f9836f7b0af4
Matej Buljan <matejb6@icloud.com>
2024-11-20 22:54:02 +0100

    Add tests for not found page

--- src/app/pages/not-found-page/not-found-page.component.spec.ts
+++ src/app/pages/not-found-page/not-found-page.component.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundPageModule } from './not-found-page.module';
 import { NotFoundPageComponent } from './not-found-page.component';
+import { NotFoundPageComponentQuery } from './not-found-page.component.query.spec';
 
 describe('NotFoundPageComponent', () => {
   let fixture: ComponentFixture<NotFoundPageComponent>;
   let component: NotFoundPageComponent;
+  let nativeElem: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,9 +17,20 @@ describe('NotFoundPageComponent', () => {
     fixture = TestBed.createComponent(NotFoundPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    nativeElem = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render message', () => {
+    expect(NotFoundPageComponentQuery.getMessage(nativeElem)).toBeTruthy();
+  });
+
+  it('should have message', () => {
+    expect(NotFoundPageComponentQuery.getMessage(nativeElem)?.innerText).toContain(
+      'URL you try to access is not available'
+    );
   });
 });
