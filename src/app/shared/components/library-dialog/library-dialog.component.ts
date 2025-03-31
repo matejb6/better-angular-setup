7744ed262a9f14b954c9a368de4a454ab22cccdd
Matej Buljan <matejb6@icloud.com>
2025-03-31 16:41:50 +0200

    Migrate to inject()

--- src/app/shared/components/library-dialog/library-dialog.component.ts
+++ src/app/shared/components/library-dialog/library-dialog.component.ts
@@ -1,10 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-
-import { Library } from '@app/core/interfaces';
 
 @Component({
   selector: 'app-library-dialog',
@@ -14,5 +12,5 @@ import { Library } from '@app/core/interfaces';
   styleUrl: './library-dialog.component.scss'
 })
 export class LibraryDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public libraryCard: Library) {}
+  public libraryCard = inject(MAT_DIALOG_DATA);
 }
